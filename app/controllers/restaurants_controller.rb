370f64f50b7321ff646b34d1a983f7a81f313845
Bruno Masuda <brunokazuaki@gmail.com>
2019-10-10 21:49:03 -0300

    half done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,20 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    set_restaurant
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'restaurants', to: 'restaurants#index'
+
+  get 'restaurants/new', to: 'restaurants#new'
+
+  get 'restaurants/show', to: 'restaurants#show'
 end
